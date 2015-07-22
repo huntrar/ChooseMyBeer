972148ed44087f90bf0fabe3f4215c1939cfe0d8
Hunter H <huntrar@gmail.com>
2015-07-21 22:50:54 -0700

    BevMo data is now saved in a list of Beer objects

---
+++ beer.py
@@ -0,0 +1,82 @@
+
+import sys
+
+from alc_reference import get_alc_reference
+from utils import get_html, is_num, unique
+
+
+
+class Beer(object):
+    ''' Self-parsing Beer keg class '''
+    def __init__(self, url, verbose=False):
+        ''' url must be a string containing the url for a single BevMo keg
+
+            verbose is a flag that prints the contents of the Beer object during parsing
+        
+            parse(url) retrieves the page and parses the contents into the following members
+
+                self.name    (may include brewery/brand and/or beer)
+                self.price
+                self.volume
+                self.num_avail
+                self.desc
+        '''
+        self.url = url
+        self.verbose = verbose
+        self.parse(url)
+
+
+    def open(self):
+        webbrowser.open(url)
+
+
+    def parse(self, url):
+        html = get_html(url)
+
+        ''' Attempt to get name and volume and return if either fails '''
+        try:
+            self.name = html.xpath('//h1/text()')[0].strip()
+            if '(' in self.name and ')' in self.name:
+                    split_name = self.name.split('(')
+                    self.name = split_name[0].strip()
+                    self.volume = float(filter(lambda x: is_num(x.replace('.', '')), split_name[1].strip(')').strip()))
+
+                    if self.verbose:
+                        sys.stderr.write(self.name + ', ')
+                        sys.stderr.write(str(self.volume) + ', ')
+            else:
+                ''' Failed to retrieve volume, which is necessary for computation '''
+                sys.stderr.write('Failed to retrieve volume!')
+                return
+        except Exception as e:
+            assert e
+            
+        ''' Attempt to get price and return if it fails '''
+        try:
+            self.price = float(html.xpath('//span[@class="ProductDetailItemPrice"]/text()')[0].strip())
+
+            if self.verbose:
+                sys.stderr.write(str(self.price) + ', ')
+        except Exception as e:
+            assert e
+       
+        ''' Attempt to get number of available kegs '''
+        try:
+            self.num_avail = int(html.xpath('//em/text()')[0].strip().split()[0])
+
+            if self.verbose:
+                sys.stderr.write(str(self.num_avail) + ', ')
+        except Exception as e:
+            self.num_avail = ''
+        
+        ''' Attempt to get description '''
+        try:
+            self.desc = html.xpath('//td[@class="ProductDetailCell"]/p/text()')[0].strip()
+
+            if self.verbose:
+                sys.stderr.write(self.desc + '\n')
+        except Exception as e:
+            self.desc = ''
+
+
+
--- choosemybeer.py
+++ choosemybeer.py
@@ -13,23 +13,55 @@ import requests
 import webbrowser
 
 from alc_reference import get_alc_reference
-from keg import Keg
+from beer import Beer
 from utils import get_html, is_num, unique
 
 import lxml.html as lh
 
 
 def get_bevmo_beers():
-    seed_url = "http://www.bevmo.com/Shop/ProductList.aspx/Beer/Kegs/_/N-15Z1z141vn?DNID=Beer"
-    seed_html = get_url(seed_url)
+    '''     For info on XPaths, see:
 
-    ''' Get page links from seed_url
-
-        For info on XPaths, see:
             http://www.w3schools.com/xpath/xpath_syntax.asp
     '''
-    links = seed_html.xpath('//div[@class="ProductListPaging"]/a/@href')
+    seed_url = "http://www.bevmo.com/Shop/ProductList.aspx/Beer/Kegs/_/N-15Z1z141vn?DNID=Beer"
+    base_url = '{url.scheme}://{url.netloc}'.format(url=urlparse(seed_url))
 
+    ''' Unique new page links with their base url appended '''
+    new_page_links = []
+    new_page_links[:] = unique(get_html(seed_url).xpath('//div[@class="ProductListPaging"]/a/@href'))
+    page_links = map(lambda x: base_url + x, new_page_links)
+
+    ''' Lists for holding links to individual beer kegs '''
+    new_beer_links = []
+    beer_links = []
+
+    ''' List to hold Beer keg objects '''
+    beers = []
+    while len(page_links) > 0:
+        ''' Links are removed as they are crawled '''
+        page_link = page_links.pop(0)
+
+        ''' Beer keg links '''
+        new_beer_links[:] = unique(get_html(page_link).xpath('//a[@class="ProductListItemLink"]/@href'))
+        beer_links += map(lambda x: base_url + x, new_beer_links)
+        for link in beer_links:
+            beers.append(Beer(link, verbose=True))
+
+        try:
+            ''' A typical link looks like Shop/ProductList.aspx/_/N-15Z1z141vn/No-100?DNID=Beer
+
+                If the number following No- is evenly divisible by 100 it leads to more pages
+
+            '''
+            if 'No-' in page_link and (page_link.split('No-')[1].split('?')[0] % 100) == 0:
+                ''' Unique new page links with their base url appended '''
+                new_page_links[:] = unique(get_html(page_link).xpath('//div[@class="ProductListPaging"]/a/@href'))
+                page_links += map(lambda x: base_url + x, new_page_links)
+        except Exception as e:
+            assert e
+
+    return beers
 
 
 def run():
@@ -37,12 +69,27 @@ def run():
 
         Access alcohol percentage using alc_ref['brand']['beer']
     '''
-    alc_ref = get_alc_reference()
+    #alc_ref = get_alc_reference()
 
     ''' Currently prints the contents of the alcohol reference dict '''
+    '''
     for brand in alc_ref.iterkeys():
         for beer in alc_ref[brand].iterkeys():
             print brand + ',', beer + ',', alc_ref[brand][beer] + '%'
+    '''
+
+    ''' members in Beer object:
+
+            self.name    (may include brewery/brand and/or beer)
+            self.price
+            self.volume
+            self.num_avail
+            self.desc
+    '''
+    beers = get_bevmo_beers()
+    print 'Printing beers'
+    for b in beers:
+        print b.name, b.price, b.volume, b.num_avail, b.desc 
 
 
 
