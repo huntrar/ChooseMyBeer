73b1eb88e64a8105f9630bd8d9461b7f892690bc
Hunter H <huntrar@gmail.com>
2015-07-26 23:00:21 -0700

    changed google to bing and made abv lookups for unique domains only

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ChooseMyBeer
 
 ## find the keg that's right for you
-ChooseMyBeer is intended to calculate the [BevMo beer kegs](http://www.bevmo.com/Shop/ProductList.aspx/Beer/Kegs/_/N-15Z1z141vn?DNID=Beer) with the highest alcohol volume per USD. The alcohol percentage for each keg is found by using the top result from google. The user is given a menu to choose from the top kegs, accompanied by their volume in gallons, price, availability, and description. Choosing a keg will open the relevant BevMo page in your browser.
+ChooseMyBeer is intended to calculate the [BevMo beer kegs](http://www.bevmo.com/Shop/ProductList.aspx/Beer/Kegs/_/N-15Z1z141vn?DNID=Beer) with the highest alcohol volume per USD. The alcohol percentage for each keg is found by using the top result from bing. The user is given a menu to choose from the top kegs, accompanied by their volume in gallons, price, availability, and description. Choosing a keg will open the relevant BevMo page in your browser.
 
 ## Installation
 * `git clone https://github.com/huntrar/ChooseMyBeer`
--- beerkeg.py
+++ beerkeg.py
@@ -1,6 +1,7 @@
 
 import re
 import webbrowser
+from urlparse import urlparse
 
 from utils import get_text, get_html, is_num, unique
 
@@ -89,40 +90,81 @@ class BeerKeg(object):
 
 
     def get_abv(self):
-        ''' Attempts to find alcohol percentage using google first result '''
+        ''' Attempts to find alcohol percentage using bing first result '''
         abv = ''
 
         ''' A ceiling for ABV content for validation
         
             We can assume BevMo does not offer kegs with this high of an alcohol content
         '''
-        max_abv = 30.0
+        max_abv = 20.0
         
         if not self.parsed:
             self.parse()
 
-        search_url = 'https://www.google.com/search?q=' + '+'.join(self.name.split()) + '+alcohol+content'
+        search_url = 'https://www.bing.com/search?q=' + '+'.join(self.name.split()) + '+alcohol+content'
+        #search_links = filter(lambda x: 'bing.com' not in x, get_html(search_url).xpath('//a/@href'))
         search_links = get_html(search_url).xpath('//a/@href')
-        first_link = search_links[search_links.index('#'):][1]
 
-        ''' Search the first link on google for the alcohol content using a regex '''
-        search_text = ''.join(get_text(get_html(first_link)))
+        '''
+        print 'search links is '
+        for link in search_links:
+            print link
+        print('')
+        '''
+        results = filter(lambda x: x != '#', search_links[search_links.index('javascript:'):][1:])
 
-        ''' Retrieves partial string containing the words ABV and a % '''
-        abv = re.search('(?<=[Aa][Bb][Vv])[^\d]*(\d+[.]?\d*)(?=%)|(?<=%)[^\d]*(\d+[.]?\d*)[^\d]*(?=[Aa][Bb][Cc])', search_text)
-        if abv:
-            abv = abv.group()
+        ''' Search up to num_searches of the first results on bing for the alcohol content using a regex '''
+        num_searches = 3
 
-            ''' Filters for a number with or without a decimal pt '''
-            abv = float(re.search('(\d+[.]?\d*)', abv).group())
+        ''' Filter links with same domain to improve chances of matching '''
+        searched_domains = set()
+        
+        ''' Add the top page results that are unique, r_it is an iterator '''
+        top_results = []
+        r_it = 0
+        result_link = ''
 
-            if abv < max_abv:
+        while len(top_results) < num_searches:
+            result_link = results[r_it]
+            domain = '{url.netloc}'.format(url=urlparse(result_link))
+            if '.' in domain:
+                if domain.count('.') > 1:
+                    domain = domain.split('.')[1]
+                else:
+                    domain = domain.split('.')[0]
+
+            if domain in searched_domains:
+                r_it += 1
+            else:
+                top_results.append(result_link)
+                r_it += 1
+                searched_domains.add(domain)
+
+        for i in xrange(num_searches):
+            try:
+                search_text = ''.join(get_text(get_html(top_results[i])))
+            except Exception:
+                continue
+
+            ''' Retrieves partial string containing the words ABV and a % '''
+            abv = re.search('(?<=[Aa][Bb][Vv])[^\d]*(\d+[.]?\d*)(?=%)|(?<=%)[^\d]*(\d+[.]?\d*)[^\d]*(?=[Aa][Bb][Cc])', search_text)
+            if abv:
+                abv = abv.group()
+
+                ''' Filters for a number with or without a decimal pt '''
+                abv = float(re.search('(\d+[.]?\d*)', abv).group())
+
+                if abv < max_abv:
+                    if self.verbose:
+                        print('ABV for {} is {}'.format(self.name, abv))
+
+                    return abv
+            else:
                 if self.verbose:
-                    print('ABV for {} is {}'.format(self.name, abv))
+                    print('No ABV found on {}'.format(top_results[i]))
 
-                return abv
-
-        ''' If we've reached this point there is no match, so return None '''
+        ''' No ABV was found by this point '''
         if self.verbose:
             print('ABV not found for {}'.format(self.name))
 
